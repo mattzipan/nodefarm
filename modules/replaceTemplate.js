a8cf0d1ffe5f461e0cd23813a6611cfa6a2cfeb1
mattzipan <mattzipan@gmail.com>
2020-01-06 05:53:18 +0800

    replaceTemplate in its own module

---
+++ modules/replaceTemplate.js
@@ -0,0 +1,14 @@
+module.exports = replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%ORIGIN%}/g, product.from);
+  output = output.replace(/{%PRODUCTNUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%PRODUCTQUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
